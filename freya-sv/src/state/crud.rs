4ee23ca124f94648c94f9f3c14fab2923a712e79
ibro <sameoldlab@proton.me>
2025-09-15 22:44:05 +0100

    crud manager

--- freya-sv/src/state/crud.rs
+++ freya-sv/src/state/crud.rs
@@ -26,3 +26,49 @@ impl std::fmt::Display for Entry {
     }
 }
 
+pub struct Crud {
+    items: Vec<Entry>,
+    firstname_: String,
+    lastname_: String,
+    filter: String,
+    selected: usize,
+}
+
+impl Default for Crud {
+    fn default() -> Self {
+        Self {
+            items: vec![
+                Entry::new("Hans".to_string(), "Emil".to_string()),
+                Entry::new("Max".to_string(), "Musterman".to_string()),
+                Entry::new("Roman".to_string(), "Tisch".to_string()),
+            ],
+            firstname_: String::with_capacity(20),
+            lastname_: String::with_capacity(20),
+            filter: String::with_capacity(20),
+            selected: 0,
+        }
+    }
+}
+
+impl Crud {
+    fn next(&mut self) {
+        self.selected = (self.selected + 1).max(self.items.len() - 1)
+    }
+    fn prev(&mut self) {
+        self.selected = (self.selected - 1).min(0)
+    }
+    fn create(&mut self, entry: Entry) {
+        self.items.push(entry)
+    }
+    fn update(&mut self) {
+        if !self.firstname_.is_empty() {
+            self.items[self.selected].firstname = self.firstname_.clone();
+        }
+        if !self.lastname_.is_empty() {
+            self.items[self.selected].lastname = self.lastname_.clone();
+        }
+    }
+    fn delete(&mut self) {
+        self.items.swap_remove(self.selected);
+    }
+}
